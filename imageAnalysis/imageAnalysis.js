ccff7444007a49cf16eace4ce008b7af873fed68
ZevCe <cellaz@rpi.edu>
2025-10-14 17:03:20 -0400

    new way of formatting pathing

--- imageAnalysis/imageAnalysis.js
+++ imageAnalysis/imageAnalysis.js
@@ -1,21 +1,24 @@
-const {webUtils } = require('electron');
-const { createWorker } = require('tesseract.js');
-
+const {webUtils} = require('electron');
+const {createWorker} = require('tesseract.js');
+const path = require('path');
 
 async function performOCR(imagePath) {
 
   //const worker = await createWorker(`C:\\Users\\cellaz\\Documents\\GitHub\\DMPO-AZ\\imageAnalysis\\eng.traineddata`);
   
-  const worker = createWorker({
-    cachePath: __dirname,
+  const worker = await createWorker("eng", 1, {
+    //temporary solution, eventually do this without absolute paths
+    workerPath: path.join(__dirname, '../node_modules/tesseract.js/dist/worker.min.js'),
+    corePath: path.join(__dirname, '../node_modules/tesseract.js-core/'),
+    langPath: path.join(__dirname, './'), // directory containing eng.traineddata
     logger: m => console.log(m),
+    // Disable gzip since the eng.traineddata file we are using is already uncompressed
+    gzip: false,
+    workerBlobURL: false
   });
   
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
-
-  const ret = await worker.recognize(imagePath);
-  console.log(ret.data.text);
+  const { data: { text } } = await worker.recognize(imagePath);
+  console.log(text);
   await worker.terminate();
 
 }
@@ -25,10 +28,9 @@ document.getElementById("imageSelector").addEventListener("change", (event) => {
     const fileList = event.target.files; 
 
     for (const file of fileList) {
-      const path = webUtils.getPathForFile(file);
-      console.log(file, path, __dirname);
-      performOCR(path);
-        
+      const filePath = webUtils.getPathForFile(file);
+      console.log(file, filePath, __dirname);
+      performOCR(filePath);
     }
 });
 
